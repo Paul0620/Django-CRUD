ed61c6c0ed49bcf55d1150e0c72ebf2346018579
Paul <79445646+Paul0620@users.noreply.github.com>
2022-02-04 23:34:08 +0900

    serializers

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+# 회원가입
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "password", "nickname"]
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data["email"], nickname=validated_data["nickname"]
+        )
+        # .set_password(raw_password) : 지정 암호를 암호화해서 password 필드에 저장 (save함수 호출 안함)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
