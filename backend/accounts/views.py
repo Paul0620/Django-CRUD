babe4120ce9de667b635e8a46f19f364381d352a
Paul <79445646+Paul0620@users.noreply.github.com>
2022-02-06 13:52:03 +0900

    views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import User
+from .serializers import SignupSerializer, UserSerializer
 
 # Create your views here.
 # 회원가입
