d59c738a9de393b9377fd4741289618596f78768
Paul <79445646+Paul0620@users.noreply.github.com>
2022-02-07 23:32:22 +0900

    views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,19 +1,30 @@
+from msilib.schema import ServiceInstall
 from django.http import request
+from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    get_object_or_404,
+)
 from .models import User
 from .serializers import SignupSerializer, UserSerializer
 
 # Create your views here.
 # 회원가입
-@api_view(["POST"])
-def SignupView():
-    pass
+class SignupView(CreateAPIView):
+    model = get_user_model()
+    serializer = SignupSerializer
+    permission = [AllowAny]
 
 
 # 회원정보
-@api_view(["GET"])
-def UserView(request, id):
-    user = User.objects.get(id=id)
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+class UserView(RetrieveAPIView):
+    queryset = get_user_model().objects.all()
+    serializer = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
