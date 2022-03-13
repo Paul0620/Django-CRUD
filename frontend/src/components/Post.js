6718d00fd08d6bd0d021cd26fb92fd4f7dbf8f53
Paul <79445646+Paul0620@users.noreply.github.com>
2022-03-13 23:34:39 +0900

    UPDATE Post.js

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { useAppContext } from "store";
 
@@ -15,13 +16,16 @@ function Post({ post }) {
   return (
     <div className="col-4 py-3">
       <div className="card">
-        <img src={image} className="card-img-top" />
+        <div className="card-header">{user}</div>
+        <div className="ratio ratio-4x3">
+          <img src={image} className="card-img-top" />
+        </div>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{content}</p>
-          <a href="#" className="btn btn-primary">
+          <Link href="#" className="btn btn-primary" style={{ float: "right" }}>
             자세히
-          </a>
+          </Link>
         </div>
       </div>
     </div>
