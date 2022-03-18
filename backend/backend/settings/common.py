a0b6921fb4eaf42c8d2a04d2cd829899b1f7515c
Paul <79445646+Paul0620@users.noreply.github.com>
2022-03-18 12:21:41 +0900

    DB수정4

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from backend.secrets import DATABASES, SECRET_KEY
+from backend.secrets import SECRET_KEY
 from pathlib import Path
 import os, json
 from datetime import timedelta
