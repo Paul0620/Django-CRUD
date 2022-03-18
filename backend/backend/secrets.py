2e511327617152a5fd6e9fb683854670b532de7e
Paul <79445646+Paul0620@users.noreply.github.com>
2022-03-18 12:12:06 +0900

    DB 수정3

--- backend/backend/secrets.py
+++ backend/backend/secrets.py
@@ -1,12 +1,1 @@
 SECRET_KEY = "django-insecure-whze^)ybwqb*l2^(%16nd%^g4uxi7!op9o&%ycxce9_vultlwl"
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "database-1",
-        "USER": "admin",
-        "PASSWORD": "qkfka0620",
-        "HOST": "database-1.cpyk6hitqndq.ap-northeast-2.rds.amazonaws.com",
-        "PORT": "3306",
-    }
-}
