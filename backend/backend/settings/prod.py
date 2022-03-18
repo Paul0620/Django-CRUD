ea18dfb76b42db2c31caab6b22dabedd9007c56d
Paul <79445646+Paul0620@users.noreply.github.com>
2022-03-18 11:39:28 +0900

    DB연결

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,4 +1,13 @@
 from .common import *
 
 # DB MySQL로 설정
-DATABASES = DATABASES
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "database-1",
+        "USER": "admin",
+        "PASSWORD": "qkfka0620",
+        "HOST": "database-1.cpyk6hitqndq.ap-northeast-2.rds.amazonaws.com",
+        "PORT": "3306",
+    }
+}
