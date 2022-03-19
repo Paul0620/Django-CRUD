a64a0055ba828a223b2f11171a3423620868e846
Paul <79445646+Paul0620@users.noreply.github.com>
2022-03-19 12:42:21 +0900

    s3 수정

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -153,11 +153,11 @@ AUTH_USER_MODEL = "accounts.User"  # 관리유저로 사용할 모델을 설정�
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
