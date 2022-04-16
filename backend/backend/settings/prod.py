723066af453c2ba9d92b76be7a463d2191992510
Paul <79445646+Paul0620@users.noreply.github.com>
2022-04-17 00:00:24 +0900

    수정

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -6,16 +6,7 @@ DEBUG = False
 ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com", ".crud.codes"]
 
 # DB MySQL로 설정
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "crud",
-        "USER": "admin",
-        "PASSWORD": "qkfka0620",
-        "HOST": "database-1.cpyk6hitqndq.ap-northeast-2.rds.amazonaws.com",
-        "PORT": "3306",
-    }
-}
+DATABASES = DATABASES
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
